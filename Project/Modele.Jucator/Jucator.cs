39514bb8883b00396a0b9107b51fd4b6cd5d1ab6
denisbetegh <42676965+denisbetegh@users.noreply.github.com>
2019-01-03 13:05:30 +0200

    Added the Player function and Score

---
+++ Project/Modele.Jucator/Jucator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modele.Jucator
+{
+    public class Jucator:InterfaceJucator
+    {
+        string Nume;
+        int Varsta;
+        string Pozitie;
+        bool Titular;
+        /*se vor adauga campuri ulterior*/
+
+        public string getNume { get { return Nume; } }
+        public int getVarsta { get { return Varsta; } }
+        public string getPozitie { get { return Pozitie; } }
+        public bool getTitular { get { return Titular; } }
+
+        public Jucator(string nume, int varsta, string pozitie, bool titular)
+        {
+            this.Nume = nume;
+            this.Varsta = varsta;
+            this.Pozitie = pozitie;
+            this.Titular = titular;
+        }
+    }
+}
---
+++ Project/Repository.Jucator/RJucator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Jucator
+{
+    public class RJucator
+    {
+        private static List<Modele.Jucator.Jucator> _jucator = new List<Modele.Jucator.Jucator>();
+
+        public void ADD_Jucator(Modele.Jucator.Jucator jucator)
+        {
+            var new_jucator = _jucator.FirstOrDefault(nj => nj.Equals(jucator));
+            if (new_jucator != null) throw new DuplicateWaitObjectException();
+
+            _jucator.Add(new_jucator);
+            Console.WriteLine("New match added");
+        }
+
+        public void DELETE_Jucator(Modele.Jucator.Jucator nume_jucator)
+        {
+            var del_jucator = _jucator.FirstOrDefault(dj => dj.Equals(nume_jucator));
+            _jucator.Remove(del_jucator);
+            Console.WriteLine("Player deleted");
+        }
+
+        public void UPDATE_Jucator()
+        {
+            /*Baza de Date*/
+            Console.WriteLine("Update Succesfull");
+        }
+    }
+}
