53a75977dd93b085758ab35e389598963e0b3fb5
sebibudea <44145260+sebibudea@users.noreply.github.com>
2019-01-05 19:17:14 +0200

    Add files via upload

---
+++ Project/Repository.Meci/RMeci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Meci
+{
+    public class RMeci
+    {
+        private static List<Modele.Meci.Meci> _meci = new List<Modele.Meci.Meci>();
+
+        public void ADD_Meci(Modele.Meci.Meci meci)
+        {
+            var new_meci = _meci.FirstOrDefault(nm => nm.Equals(meci));
+            if (new_meci != null) throw new DuplicateWaitObjectException();
+
+            _meci.Add(meci);
+            Console.WriteLine("New match added");
+        }
+
+        public void DELETE_Meci(Modele.Meci.Meci nume_meci)
+        {
+            var del_meci = _meci.FirstOrDefault(dm => dm.Equals(nume_meci));
+            _meci.Remove(del_meci);
+            Console.WriteLine("Match deleted");
+        }
+
+        public void UPDATE_Meci()
+        { 
+            /*Baza de Date*/
+            Console.WriteLine("Update Succesfull");
+        }
+    }
+}
---
+++ Project/Services.Meci/SMeci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Services.Meci
+{
+    public class SMeci
+    {
+
+        public void WATCH_Meci()
+        { 
+            /* Vizualizare date din Modele.Meci*/
+        }
+    }
+}
